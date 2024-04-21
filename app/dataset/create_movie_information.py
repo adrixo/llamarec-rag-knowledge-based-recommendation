e28d0d3c2ea0095542f7174faa348b325c81c0ea
adrixo <avalera@hpe.com>
2024-04-21 20:30:44 +0100

    feat(reduced): enrich with user ratings

--- app/dataset/create_movie_information.py
+++ app/dataset/create_movie_information.py
@@ -7,16 +7,21 @@ tags_dict = tags_dict.set_index('tagId')['tag'].to_dict()
 # Adds tags to the movies
 movies = pd.read_csv(dataset_path+'movies.csv')
 movies['tags'] = ""
+movies['rated_tags'] = ""
 
 scores = pd.read_csv(dataset_path+'genome-scores.csv')
-scores['tags'] = ""
+rated_movies_with_tag = pd.read_csv(dataset_path+'tags_movies_rated.csv')
 
 # Para cada pelicula
 for index, row in movies.iterrows():
     movie_id = row['movieId']
+    # For genome
     tag_ids = scores[scores['movieId'] == movie_id]['tagId'].values
     tag_ids = [tags_dict[tag_id] for tag_id in tag_ids]
     movies.at[index, 'tags'] = " ".join(tag_ids)
+    # For rated tags
+    t = rated_movies_with_tag[rated_movies_with_tag['movieId'] == movie_id]['tag'].values
+    movies.at[index, 'rated_tags'] = t
 
 # Obtenemos los Tags que le pusieron los usuarios a la hora de valorarlas
 
