9627069f9f3148a59b299867971ff316c2ef6592
adrixo <avalera@hpe.com>
2024-04-21 21:31:45 +0100

    feat(milvus): query side

--- app/query_milvus.py
+++ app/query_milvus.py
@@ -8,6 +8,7 @@ from pymilvus import (
 from sentence_transformers import SentenceTransformer
 from tqdm.auto import tqdm
 tqdm.pandas()
+import sys
 
 #####
 ##### Embed model
@@ -22,7 +23,6 @@ collection_name = "movies"
 num_entities, dim = 10000, 384
 
 connections.connect("default", host="localhost", port="19530")
-utility.drop_collection(collection_name)
 fields = [
     FieldSchema(name="pk", dtype=DataType.VARCHAR, is_primary=True, auto_id=False, max_length=100),
     FieldSchema(name="content", dtype=DataType.VARCHAR, max_length=5000),
@@ -30,36 +30,24 @@ fields = [
 ]
 schema = CollectionSchema(fields, "Collection with movies and its tags as embeddings")
 milvus_db = Collection(collection_name, schema, consistency_level="Strong")
-print(f"Collection '{collection_name}' is created: {utility.has_collection(collection_name)}")
-
-###### 
-###### Populate Milvus
-# Tomamos el dataset de las peliculas
-dataset_path = "/Users/adrianvalera/git/personal/llamarec-rag/dataset/ml-25m-reduced/"
-movies = pd.read_csv(dataset_path+'movies_enrich.csv')
-
-batch_size = 1
-page_batch_size = 10
-
-
-i = 0
-for index, row in tqdm(movies.iterrows(), total=movies.shape[0]):
-    i+=1
-    name = row['title']
-    tags = row['tags']
-    embed = model.encode(tags)
-    milvus_db.insert([[name], [tags],  [embed]])
-
-milvus_db.flush()
 
 ######
-###### Set index for queries
-milvus_db.release()
-milvus_db.drop_index()
-index = {
-    "index_type": "IVF_FLAT",
+###### Query
+
+user_input = ' '.join(sys.argv[1:])
+vectors_to_search = model.encode(user_input)
+search_params = {
     "metric_type": "COSINE",
-    "params": {"nlist": 128},
+    "params": {"nprobe": 10}
 }
-milvus_db.create_index("embeddings", index)
-milvus_db.load()
+result = milvus_db.search([vectors_to_search], "embeddings", search_params, limit=10, output_fields=["pk"])
+
+
+try:
+    for i in range(10):
+        print(i, result[0][i].entity.get('pk'))
+        #print(result[0][i].entity.get('content'))
+    #print(result[0][1].entity.get('content'))
+except Exception as e:
+    pass
+
