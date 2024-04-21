58389af9e9fcfb5d3682e53972e6019ed4c2aa1e
adrixo <avalera@hpe.com>
2024-04-21 20:34:01 +0100

    feat(populate): get dataset

--- app/populate_milvus.py
+++ app/populate_milvus.py
@@ -1,7 +1,11 @@
+import pandas as pd
+
 # Tomamos el dataset de las peliculas
+dataset_path = "/Users/adrianvalera/git/personal/llamarec-rag/dataset/ml-25m-reduced/"
+movies = pd.read_csv(dataset_path+'movies-enriched.csv')
 
 # para cada elemento
-
-# Hacemos un embedding
-
-# Lo guardamos en Milvus
+for index, row in movies.iterrows():
+    # Hacemos un embedding
+    pass
+    # Lo guardamos en Milvus
