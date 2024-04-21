bb12c2412515ff9a1b5717b93b4d1cb3ee3a554b
adrixo <avalera@hpe.com>
2024-04-21 20:57:18 +0100

    feat(milvus): pupulate the database

--- app/populate_milvus.py
+++ app/populate_milvus.py
@@ -5,13 +5,23 @@ from pymilvus import (
     FieldSchema, CollectionSchema, DataType,
     Collection,
 )
+from sentence_transformers import SentenceTransformer
+from tqdm.auto import tqdm
+tqdm.pandas()
+
+#####
+##### Embed model
+print("Loading model...")
+model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+
 
 ###### 
-###### 
+###### Milvus setup
+print("Seting up milvus...")
 collection_name = "movies"
 num_entities, dim = 10000, 384
 
-connections.connect("default", host="milvus", port="19530")
+connections.connect("default", host="localhost", port="19530")
 utility.drop_collection(collection_name)
 fields = [
     FieldSchema(name="pk", dtype=DataType.VARCHAR, is_primary=True, auto_id=False, max_length=100),
@@ -23,13 +33,25 @@ milvus_db = Collection(collection_name, schema, consistency_level="Strong")
 print(f"Collection '{collection_name}' is created: {utility.has_collection(collection_name)}")
 
 ###### 
-###### 
+###### Populate Milvus
 # Tomamos el dataset de las peliculas
 dataset_path = "/Users/adrianvalera/git/personal/llamarec-rag/dataset/ml-25m-reduced/"
-movies = pd.read_csv(dataset_path+'movies-enriched.csv')
+movies = pd.read_csv(dataset_path+'movies_enrich.csv')
 
 # para cada elemento
 for index, row in movies.iterrows():
     # Hacemos un embedding
     pass
     # Lo guardamos en Milvus
+
+
+batch_size = 1
+page_batch_size = 10
+
+
+for index, row in tqdm(movies.iterrows(), total=movies.shape[0]):
+    name = row['title']
+    tags = row['tags']
+    embed = model.encode(tags)
+
+    milvus_db.insert([[name], [tags],  [embed]])
